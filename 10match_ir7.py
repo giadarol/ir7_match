996a3d783adfe2b4171c680f17537db9692b8a4d
Riccardo De Maria <riccardodemaria@gmail.com>
2023-06-26 15:08:54 +0200

    update

--- 10match_ir7.py
+++ 10match_ir7.py
@@ -2,9 +2,38 @@ import numpy as np
 
 import xtrack as xt
 import xpart as xp
+import xdeps as xd
 
 lhc = xt.Multiline.from_json("hllhc.json")
 lhc.build_trackers()
+
+
+p1=lhc.lhcb1.build_particles(x=0,delta=1e-3)
+p2=lhc.lhcb2.build_particles(x=0,delta=1e-3)
+lhc.lhcb1.track(p1, ele_start="tcp.d6l7.b1", ele_stop="tcspm.6r7.b1")
+print(p1.x/1e-3)
+lhc.lhcb2.track(p2, ele_start="tcp.d6r7.b2", ele_stop="tcspm.6l7.b2",backtrack=True)
+print(p2.x/1e-3)
+
+class SinglePassDispersion(xd.Action):
+    def __init__(self,line,ele_start,ele_stop,backtrack=False,delta=1e-3):
+        self.line=line
+        self.ele_start=ele_start
+        self.ele_stop=ele_stop
+        self.delta=delta
+        self.backtrack=backtrack
+        self._pp=line.build_particles(delta=delta)
+
+    def run(self):
+        for nn in ['x','px','y','py','zeta','delta','at_element']:
+            setattr(self._pp,nn,0)
+        self._pp.delta=self.delta
+        self.line.track(self._pp,
+                        ele_start=self.ele_start,
+                        ele_stop=self.ele_stop,
+                        backtrack=self.backtrack)
+        return {'d'+nn:getattr(self._pp,nn)[0]/self.delta for nn in ['x','px','y','py']}
+
 
 
 tw1 = lhc.lhcb1.twiss()
@@ -34,6 +63,7 @@ lhc.match(
     vary=varylist,
     verbose=True,
 )
+
 
 
 
