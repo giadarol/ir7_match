19e250019e2d9dc7c8b067c91a75c47f1cb14021
giadarol <giovanni.iadarola@cern.ch>
2023-07-09 10:37:50 +0200

    Some timings

--- 10match_ir7.py
+++ 10match_ir7.py
@@ -1,3 +1,5 @@
+import time
+
 import re
 import numpy as np
 
@@ -5,9 +7,12 @@ import xtrack as xt
 import xpart as xp
 import xdeps as xd
 
+t0 = time.time()
+
 lhc = xt.Multiline.from_json("hllhc.json")
 lhc.build_trackers()
 
+t1 = time.time()
 
 p1 = lhc.lhcb1.build_particles(x=0, delta=1e-3)
 p2 = lhc.lhcb2.build_particles(x=0, delta=1e-3)
@@ -16,6 +21,7 @@ print(p1.x / 1e-3)
 lhc.lhcb2.track(p2, ele_start="tcp.d6r7.b2", ele_stop="tcspm.6l7.b2", backtrack=True)
 print(p2.x / 1e-3)
 
+t2 = time.time()
 
 class SinglePassDispersion(xd.Action):
     def __init__(self, line, ele_start, ele_stop, backtrack=False, delta=1e-3):
@@ -41,21 +47,27 @@ class SinglePassDispersion(xd.Action):
             for nn in ["x", "px", "y", "py"]
         }
 
+t3 = time.time()
+
 
 tw1 = lhc.lhcb1.twiss()
 tw2 = lhc.lhcb2.twiss()
 assert np.allclose(tw1["betx", "ip5"], 0.5)
 assert np.allclose(tw2["betx", "ip5"], 0.5)
+
 bir7b1 = tw1.get_twiss_init("s.ds.l7.b1")
 eir7b1 = tw1.get_twiss_init("e.ds.r7.b1")
 bir7b2 = tw2.get_twiss_init("s.ds.l7.b2")
 eir7b2 = tw2.get_twiss_init("e.ds.r7.b2")
+
+t4 = time.time()
 
 from pyoptics import madlang
 newopt=madlang.load(open("ir7_optics5_2.str")).vars_to_dict()
 for k,v in newopt.items():
     lhc.vars[k]=v
 
+t5 = time.time()
 
 namelist=sorted([nn for nn in lhc.vars.keys() if re.match(r"kq[t0-9].*\.[lr]7", nn)])
 varylist = [ xt.Vary(nn, step=1e-6) for nn in namelist ]
@@ -93,6 +105,9 @@ tarlist_sp = [
         xt.Target(action=act_sp2, tar="dx", value=-0.03126, tol=1e-7),
     ]
 
+t6 = time.time()
+
+
 out=lhc.match(
     ele_start=("s.ds.l7.b1", "s.ds.l7.b2"),
     ele_stop=("e.ds.r7.b1", "e.ds.r7.b2"),
@@ -104,6 +119,7 @@ out=lhc.match(
     assert_within_tol=False
 )
 
+t7 = time.time()
 
 for tt in out['optimizer']._err.targets:
     if tt.line:
@@ -121,3 +137,4 @@ for nn,vv in zip(varylist,out['res']):
     print(f"{nn.name:15} {vv}")
 
 
+t = [t1-t0, t2-t1, t3-t2, t4-t3, t5-t4, t6-t5, t7-t6]
